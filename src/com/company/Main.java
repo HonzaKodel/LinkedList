d0ddace89e07959d8ab6b0c2dfb7eda611bff324
jan.kodel <jan.kodel@sos-jh.cz>
2019-10-04 09:56:01 +0200

    hey

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import javafx.scene.control.ChoiceDialog;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Uzel main = initSeznam(5);
+    }
+    public static Uzel initSeznam(int delkaSeznamu){
+        Uzel prvniUzel = new Uzel(1);
+        Uzel posledniUzel = new Uzel(5);
+
+        for (int i = 2; i <= delkaSeznamu; i++) {
+            prvniUzel.getFront();
+            posledniUzel.getLast();
+        }
+        return prvniUzel;
     }
 }
+
+
+
+
